e50e6f97ff464de652c0073e93b482cf5ccb39ce
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2021-12-30 15:29:44 +0000

    intial commit

--- js/script.js
+++ js/script.js
@@ -143,6 +143,22 @@ Create the `searchFilter` function
 This function will filter through the students by name displaying the cards with the name in the search term
 */
 
+// const search = document.querySelector('#search');
+// const photoTexts = document.querySelectorAll('[data-caption]');
+
+// const searchBar = event => {
+//     const searchTerm = event.target.value.toLowerCase();
+//     photoTexts.forEach(photoTexts => {
+//         const text = photoTexts.getAttribute('data-caption').toLowerCase();
+//         if(text.includes(searchTerm)) {
+//             photoTexts.style.display = "block";
+//         } else {
+//             photoTexts.style.display = "none";
+//         }
+//     });
+// }
+
+// search.addEventListener('keyup', searchBar);
 
 // Call functions
 showPage(data,1);
